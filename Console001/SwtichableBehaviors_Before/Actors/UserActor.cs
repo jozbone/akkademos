9db73617fe7107b6d2e0606376a5d403e2e5c0dd
JB Osborne <jozbone@live.com>
2015-10-10 17:22:14 -0400

    Adding SwitchableBehaviors

---
+++ Console001/SwtichableBehaviors_Before/Actors/UserActor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Akka.Actor;
+using SwtichableBehaviors_Before.Messages;
+
+namespace SwtichableBehaviors_Before.Actors
+{
+    public class UserActor : ReceiveActor
+    {
+        private string _currentlyWatching;
+
+        public UserActor()
+        {
+            Console.WriteLine("Creating a UserActor");
+            Receive<PlayMovieMessage>(message => HandlePlayMovieMessage(message));
+            Receive<StopMovieMessage>(message => HandleStopMovieMessage());
+        }
+
+        private void HandlePlayMovieMessage(PlayMovieMessage message)
+        {
+            if (_currentlyWatching != null)
+            {
+                ColorConsole.WriteLineRed("Error: cannot start playing another movie before stopping existing one");
+            }
+            else
+            {
+                StartPlayingMovie(message.MovieTitle);
+            }
+        }
+
+        private void StartPlayingMovie(string title)
+        {
+            _currentlyWatching = title;
+            ColorConsole.WriteLineYellow($"User is currently watching '{_currentlyWatching}'");
+        }
+
+        private void HandleStopMovieMessage()
+        {
+            if (_currentlyWatching == null)
+            {
+                ColorConsole.WriteLineRed("Error: cannot stop if nothing is playing");
+            }
+            else
+            {
+                StopPlayingCurrentMovie();
+            }
+        }
+
+        private void StopPlayingCurrentMovie()
+        {
+            ColorConsole.WriteLineYellow($"User has stopped watching '{_currentlyWatching}'");
+            _currentlyWatching = null;
+        }
+
+        protected override void PreStart()
+        {
+            ColorConsole.WriteLineGreen("PlaybackActor PreStart");
+        }
+
+        protected override void PostStop()
+        {
+            ColorConsole.WriteLineGreen("PlaybackActor PostStop");
+        }
+
+        protected override void PreRestart(Exception reason, object message)
+        {
+            ColorConsole.WriteLineGreen($"PlaybackActor PreRestart [reason:{reason}]");
+            base.PreRestart(reason, message);
+        }
+
+        protected override void PostRestart(Exception reason)
+        {
+            ColorConsole.WriteLineGreen($"PlaybackActor PostRestart [reason:{reason}]");
+            base.PostRestart(reason);
+        }
+    }
+}
---
+++ Console001/SwtichableBehaviors_Before/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Akka.Actor;
+using SwtichableBehaviors_Before.Actors;
+using SwtichableBehaviors_Before.Messages;
+
+namespace SwtichableBehaviors_Before
+{
+    class Program
+    {
+        private static ActorSystem MovieStreamingActorSystem;
+
+        static void Main(string[] args)
+        {
+            MovieStreamingActorSystem = ActorSystem.Create("MovieStreamingActorSystem2");
+            Console.WriteLine("Actor system created.");
+
+            Props userProps = Props.Create<UserActor>();
+            IActorRef actorRef = MovieStreamingActorSystem.ActorOf(userProps, "UserActor");
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a PlayMovieMessage (Codenan the Destroyer)");
+            actorRef.Tell(new PlayMovieMessage("Codenan the Destroyer", 42));
+
+            Console.ReadKey();
+            Console.WriteLine("Sending another PlayMovieMessage (Boolean Lies)");
+            actorRef.Tell(new PlayMovieMessage("Boolean Lies", 42));
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a StopMovieMessage");
+            actorRef.Tell(new StopMovieMessage());
+
+            Console.ReadKey();
+            Console.WriteLine("Sending another PlayMovieMessage (Boolean Lies)");
+            actorRef.Tell(new PlayMovieMessage("Boolean Lies", 42));
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a StopMovieMessage");
+            actorRef.Tell(new StopMovieMessage());
+
+            Console.ReadKey();
+            Console.WriteLine("Sending a StopMovieMessage");
+            actorRef.Tell(new StopMovieMessage());
+
+            // Tell actor system (and all child actors) to shutdown
+            MovieStreamingActorSystem.Shutdown();
+            // Wait for actor system to finish shutting down
+            MovieStreamingActorSystem.AwaitTermination();
+            Console.WriteLine("Actor system has shutdown");
+
+            Console.ReadKey();
+        }
+    }
+}
